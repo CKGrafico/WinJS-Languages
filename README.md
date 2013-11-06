c93c0a422721175b7889bbd40e8bbc04aa3a5bb2
CKGrafico <enriquefegu@gmail.com>
2013-11-06 23:58:17 +0100

    init

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
-WinJS-Languages-Helper
-======================
 
-Helper for use languages in WinJS
+WinJS Languages Helper
+=====================
+
+
+Helper to more easy the languages in WinJS
+
+----------
+
+#### Examples
+
+**Include #default.html**
+
+    ...
+    <script src="Scripts/WinjsLanguages.js"></script>
+    ...
+
+**Creating strings files**
+
+    | Yourproject
+                || strings
+                        || es-ES
+                                ||resources.resjson
+                        || en-US
+                                ||resources.resjson
+**Example Strings en-US**
+
+    {
+        "Hello": "Hello",
+        "Day": "Day"
+    }
+
+**Example Strings es-ES**
+
+    {
+        "Hello": "Hola",
+        "Day": "Día"
+    }
+
+**Getter and setter**
+
+    (function (g, languages) {
+         var currentLang = languages.getLanguage();
+         languages.setLanguage('es-ES');
+    })(window, window.languages);
+
+**Translating**
+
+    (function (g, t) {
+         var hi = t('Hello!')
+    })(window, window.translate);
+
+**Optional Handlebars**
+
+    <div>{{i18n 'Hello'}}</div>
---
+++ WinjsLanguages.js
@@ -0,0 +1,54 @@
+﻿/*
+    This is a simple library to help you managing languages on Windows 8.1 with WinJS
+    author: @CKGrafico
+    version: 0.1
+*/
+
+
+(function (global) {
+
+    'use strict';
+    var applicationData = Windows.Storage.ApplicationData.current;
+    var roamingSettings = applicationData.roamingSettings;
+    var resourceContext = Windows.ApplicationModel.Resources.Core.ResourceContext.getForCurrentView();
+
+    //Language Class
+    var Languages = WinJS.Class.define(
+        // Constructor
+        function () {
+            this.setLanguage(roamingSettings.values['AppLanguage'] || resourceContext.languages[0] || 'en-US');
+        },
+
+        // Instance Members
+        {
+            translate: function (string) {
+                var translated = WinJS.Resources.getString(string);
+                if (!translated.empty) {
+                    return translated.value;
+                }
+                return '*' + string;
+            },
+
+            setLanguage: function (language) {
+
+                Windows.Globalization.ApplicationLanguages.primaryLanguageOverride = language;
+                roamingSettings.values['AppLanguage'] = language;
+                this.language = language;
+            },
+
+            getLanguage: function () {
+                return this.language;
+            }
+
+        }
+    );
+
+    global.languages = new Languages();
+    global.translate = languages.translate;
+
+    if(Handlebars) {
+        Handlebars.registerHelper('i18n', translate);
+    }
+
+
+})(window);
